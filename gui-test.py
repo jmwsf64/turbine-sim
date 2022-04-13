d0402cc89a41eee9704b4b4182f6736293f1ff32
jmwil <j.m.wilson6464@gmail.com>
2022-04-12 20:35:59 -0600

    files at endpoints, converted to test scripts

--- gui-test.py
+++ gui-test.py
@@ -2,34 +2,28 @@ from kivy.app import App
 from kivy.uix.button import Button
 from plyer import filechooser
 
-# ======================================================================================================================
-# example stl file import
-# ======================================================================================================================
-stlIn = M_STL.stlInput()
-box = stlIn.importSTL('lib/files/box.stl')
 
-
-# ======================================================================================================================
+# ============================
 # kivy application development
-# ======================================================================================================================
-
-# -------------------
-# hello world example
-# -------------------
+# ============================
 class MyApp(App):
 
     def build(self):
-        # self.icon = r'lib/icons/app-icon.png'
+        self.icon = r'lib/files/icons/spin-sim.png'
         return Button(text='Hello World')
 
 
 if __name__ == '__main__':
-    # MyApp().run()
+
+    # -------------------
+    # hello world example
+    # -------------------
+    MyApp().run()
 
     # -------------------
     # file select example
     # -------------------
-    path = filechooser.open_file(title="Choose an STL file",
-                                 filters=[("Stereo-lithography file (.stl)", "*.stl")],
-                                 multiple=True)
-    print(path)
+    # path = filechooser.open_file(title="Choose an STL file",
+    #                              filters=[("Stereo-lithography file (.stl)", "*.stl")],
+    #                              multiple=True)
+    # print(path)
--- stl-test.py
+++ stl-test.py
@@ -1,8 +1,19 @@
-from lib.modules.readers import classSTL as M_STL
+from lib.modules.readers import classSTLReader as C_STL
 
 
-# ======================================================================================================================
+# =======================
 # example stl file import
-# ======================================================================================================================
-stlIn = M_STL.stlInput()
+# =======================
+stlIn = C_STL.stlInput()
 box = stlIn.importSTL('lib/files/box.stl')
+
+# ===================
+# imported properties
+# ===================
+print('\n\nNormals: \n', box.normals)
+print('\n\nVolume:  ', box.volume)
+print('\n\nx-coords:\n', box.v0)
+print('\n\ny-coords:\n', box.v1)
+print('\n\nz-coords:\n', box.v2)
+print('\n\nCOG:     \n', box.cog)
+print('\n\nInertia: \n', box.inertia)
