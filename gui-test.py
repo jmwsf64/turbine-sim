514401fa56f67956c938e13de1208b1c84d667e5
jmwil <j.m.wilson6464@gmail.com>
2022-04-20 17:56:03 -0600

    simple app finished

--- gui-test.py
+++ gui-test.py
@@ -1,8 +1,14 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
-import lib.modules.gui.guiModule as guiF
+from kivy.config import Config
+import lib.modules.gui.guiModule as guiM
+
+
+# --------------------------------------------
+# configure images on buttons to be resizeable
+# --------------------------------------------
+Config.set('graphics', 'resizable', True)
 
 
 # ======================
@@ -10,31 +16,57 @@ import lib.modules.gui.guiModule as guiF
 # ======================
 class SpinSimApp(App):
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.bladeFiles   = []
+        self.supportFiles = []
+        self.icon = 'lib/files/icons/spin-sim.png'
+
     def build(self):
 
         # ------------------------
         # create layout parameters
         # ------------------------
-        sizeX = 800
-        sizeY = 800
-        layout = FloatLayout(size=(sizeX, sizeY))
+        layout = GridLayout(rows=2, cols=2)
 
         # --------------
         # create buttons
         # --------------
-        btn1 = Button(text='Hello World!',
-                      size_hint=(0.15, 0.1),
-                      pos=(0, 300))
-        btn2 = Button(text='Select an STL file',
-                      size_hint=(0.35, 0.1),
-                      pos=(500, 0),
-                      on_press=guiF.chooseStl)
+        btn1 = Button(text='Select Blade STL File',
+                      size_hint_x=None,
+                      width=150,
+                      size_hint_y=None,
+                      height=25,
+                      on_release=lambda button: guiM.chooseBladeStl('btn1', self.bladeFiles))
+
+        btn2 = Button(text='Print Blade STL Path',
+                      size_hint_x=None,
+                      width=150,
+                      size_hint_y=None,
+                      height=25,
+                      on_release=lambda button: guiM.printStl('btn2', self.bladeFiles))
+
+        btn3 = Button(text='Select Blade STL File',
+                      size_hint_x=None,
+                      width=150,
+                      size_hint_y=None,
+                      height=25,
+                      on_release=lambda button: guiM.chooseBladeStl('btn1', self.supportFiles))
+
+        btn4 = Button(text='Print Blade STL Path',
+                      size_hint_x=None,
+                      width=150,
+                      size_hint_y=None,
+                      height=25,
+                      on_release=lambda button: guiM.printStl('btn2', self.supportFiles))
 
         # ---------------------
         # add buttons to layout
         # ---------------------
         layout.add_widget(btn1)
         layout.add_widget(btn2)
+        layout.add_widget(btn3)
+        layout.add_widget(btn4)
 
         # -------------
         # return layout
--- lib/modules/gui/guiModule.py
+++ lib/modules/gui/guiModule.py
@@ -1,8 +1,19 @@
 from plyer import filechooser
 
 
-def chooseStl(btn):
-    path = filechooser.open_file(title="Choose an STL file",
+def chooseBladeStl(btn, a_path):
+    file = filechooser.open_file(title="Choose an STL file",
                                  filters=[("Stereo-lithography file (.stl)", "*.stl")],
                                  multiple=True)
-    return path
+    a_path.extend(file)
+
+
+def chooseSupportStl(btn, a_path):
+    file = filechooser.open_file(title="Choose an STL file",
+                                 filters=[("Stereo-lithography file (.stl)", "*.stl")],
+                                 multiple=True)
+    a_path.extend(file)
+
+
+def printStl(btn, a_path):
+    print(a_path)
